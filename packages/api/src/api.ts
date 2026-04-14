17cdccb114ecdc271eb0dba922277c6e8b9c69d6
Ismael Abarkan <i.abarkan@live.nl>
2026-04-14 18:43:12 +0200

    Revert "fix: agents aanroepen als async functies"
    This reverts commit 4bf223424c955a07380de0ec50d5d4471d9148ff.

--- packages/api/src/api.ts
+++ packages/api/src/api.ts
@@ -206,7 +206,7 @@ export const api = new Hono<{ Variables: Variables }>()
 	.post("question", vValidator("json", oneOffMessageSchema), async (c) => {
 		const { q, agent = "jw" } = c.req.valid("json");
 
-		const response = await (await agents[agent]()).query(q, [], db);
+		const response = await agents[agent].query(q, [], db);
 
 		return c.json({ response: response.message.content });
 	})
@@ -270,13 +270,13 @@ export const api = new Hono<{ Variables: Variables }>()
 					} while (modelFirst === modelSecond);
 
 					const [responseFirst, responseSecond] = await Promise.all([
-						(await agents[agent]()).query(
+						agents[agent].query(
 							inputText,
 							conversation.messages as ChatMessage[],
 							db,
 							modelFirst,
 						),
-						(await agents[agent]()).query(
+						agents[agent].query(
 							inputText,
 							conversation.messages as ChatMessage[],
 							db,
@@ -292,7 +292,7 @@ export const api = new Hono<{ Variables: Variables }>()
 					return c.json([responseFirst.message, responseSecond.message]);
 				}
 
-				const response = await (await agents[agent]()).query(
+				const response = await agents[agent].query(
 					inputText,
 					conversation.messages as ChatMessage[],
 					db,
